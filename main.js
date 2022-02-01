1a005a510c8553fe4463588576637ee6533cb4b5
Opexobov <artony138122@gmail.com>
2022-02-01 14:20:12 +0330

    complete database

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-sdcsdc
+const genRanNum = () =>{
+    return Math.floor(Math.random() * 3);
+};
+const league = [laliga, calcio, premier];
+const laliga = [real, atleti, barca];
+const calcio = [ juve, inter, milan];
+const premier = [liverpool, chelsea, city];
+const real = ['carvajal', 'assensio', 'modric']
+const atleti = ['depaul', 'suarez', 'cunha']
+const barca = ['lenglet', 'dejong', 'depay']
+const city = ['mahrez', 'debruyne', 'cancelo']
+const chelsea = ['lukaku', 'hakim', 'werner']
+const liverpool = ['gomez', 'salah', 'firmino']
+const juve = ['dybala', 'sandro', 'cuadrado']
+const inter = ['martinez', 'barella', 'vecino']
+const milan = ['zlatan', 'theo', 'maldini']
+
